8084c8d2dfc3c3942fce21876544dff05a5c487d
임은혜 <euneng356@gmail.com>
2024-07-20 00:11:33 +0900

    환경 설정

---
+++ src/test/java/com/example/codify/CodifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

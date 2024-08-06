cf80c85b91c9444dd990c5f48e5b983d94ef6bf8
unosus <kehoya@naver.com>
2024-08-06 22:16:07 +0900

    홈 주석 수정

--- diary/src/pages/Home.js
+++ diary/src/pages/Home.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../styles/Home.css';
-
-// New ImageSlider component
+//이미지 슬라이더
 const ImageSlider = () => {
     const images = ['/1.png', '/2.png', '/3.png'];
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -10,14 +9,14 @@ const ImageSlider = () => {
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 4000); // 3 seconds interval
+        }, 4000);
 
-        return () => clearInterval(interval); // Cleanup interval on component unmount
+        return () => clearInterval(interval);
     }, []);
 
     return (
         <div className='image-gallery'>
-            <img src={images[currentIndex]} alt={`Gallery Image ${currentIndex + 1}`} />
+            <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
         </div>
     );
 };
@@ -40,7 +39,7 @@ const Home = () => {
                 <h2>나만의 AI다이어리</h2>
                 <h1>Codify</h1>
                 <button onClick={() => navigate('/login')}>지금 바로 시작해보세요!</button>
-                <ImageSlider /> {/* Using the new ImageSlider component */}
+                <ImageSlider />
             </main>
         </div>
     );
